c01828456d6077dda29d02eaa131f0e4e833de46
Serhii Tereshchenko <greywolf1396@gmail.com>
2018-12-21 02:59:31 +0200

    [Migrations]

---
+++ EfDemoMigration/DemoContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using EFDemo.Models.Entities;
+
+namespace EFDemo.Migration
+{
+    class DemoContext : DbContext
+    {
+        public DemoContext() : base("DemoConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<DemoContext>());
+        }
+
+        public DbSet<Mentor> Mentors { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<JournalRecord> JournalRecords { get; set; }
+    }
+}
